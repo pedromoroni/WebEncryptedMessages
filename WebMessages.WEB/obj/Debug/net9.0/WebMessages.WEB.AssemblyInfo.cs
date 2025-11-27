0d3be81f3ce2b2b565c34a21911427b06db8b04c
Pedro Moroni <pmoroni@mainhub.pt>
2025-11-27 17:23:01 +0000

    ✨melhorias de design

--- WebMessages.WEB/obj/Debug/net9.0/WebMessages.WEB.AssemblyInfo.cs
+++ WebMessages.WEB/obj/Debug/net9.0/WebMessages.WEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebMessages.WEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6fa3a8d669afd61bcd8ad90dc00e29dc77ae27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+363c6f1a8cfa9eab3d25517430b1e2ccefd1ed1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebMessages.WEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebMessages.WEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
